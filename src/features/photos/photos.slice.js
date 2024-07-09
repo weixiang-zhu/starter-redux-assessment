a8e45fa537dfa825fba644f93f1aaacbc1302299
wzhu <wzhu@indeed.com>
2024-07-09 13:51:15 +0900

    initial commit

--- src/features/photos/photos.slice.js
+++ src/features/photos/photos.slice.js
@@ -1,9 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { selectSearchTerm } from '../search/search.slice';
 import photos from './photos.data.js';
 
 const initialState = {
-  photos,
+  photos
 };
 
 const options = {
@@ -13,10 +12,18 @@ const options = {
     // Task 1: Create an `addPhoto()` case reducer that adds a photo to state.photos. 
     // Task 1 Hint: You can use state.photos.unshift()
     // `unshift()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/unshift
-   
+    addPhoto: (state, action) => {
+      state.photos.unshift({id: state.photos.length + 1, caption: action.payload.caption, imageUrl: action.payload.imageUrl})
+    },
     // Task 6: Create an `removePhoto()` case reducer that removes a photo from state.photos
     // Task 6 Hint: You can use state.photos.splice()
     // `splice()` documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+    removePhoto: (state, action) => {
+      const index = state.photos.findIndex((photo) => photo.id === action.payload);
+      if (index !== -1) {
+        state.photos.splice(index, 1);
+      }
+    }
   },
 };
 
@@ -28,5 +35,6 @@ export default photosSlice.reducer;
 
 export const selectAllPhotos = (state) => state.photos.photos;
 export const selectFilteredPhotos = (state) => {
+  selectAllPhotos(state).filter((photo) => photo.caption.toLowerCase().includes(state.search.searchTerm.toLowerCase()));
   // Task 12: Complete `selectFilteredPhotos()` selector to return a filtered list of photos whose captions match the user's search term
 };
--- src/features/suggestion/suggestion.slice.js
+++ src/features/suggestion/suggestion.slice.js
@@ -1,7 +1,14 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 export const fetchSuggestion =
-  createAsyncThunk(/* Task 15: Complete the `createAsyncThunk()` function to load a suggestion from this URL: http://localhost:3004/api/suggestion */);
+  createAsyncThunk(/* Task 15: Complete the `createAsyncThunk()` function to load a suggestion from this URL: http://localhost:3004/api/suggestion */
+      'suggestion/fetchSuggestion',
+      async (arg, thunkAPI) => {
+        const response = await fetch('http://localhost:3004/api/suggestion');
+        const { data } = await response.json();
+        return data;
+      }
+  );
 
 const initialState = {
   suggestion: '',
@@ -15,6 +22,20 @@ const options = {
   reducers: {},
   extraReducers: {
     /* Task 16: Inside `extraReducers`, add reducers to handle all three promise lifecycle states - pending, fulfilled, and rejected - for the `fetchSuggestion()` call */
+    [fetchSuggestion.pending]: (state) => {
+      state.loading = true;
+      state.error = false;
+    },
+    [fetchSuggestion.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.error = false;
+      state.suggestion = action.payload;
+    },
+    [fetchSuggestion.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = true;
+      state.suggestion = action.payload;
+    }
   },
 };
 
@@ -23,6 +44,6 @@ const suggestionSlice = createSlice(options);
 export default suggestionSlice.reducer;
 
 // Task 17: Create a selector, called `selectSuggestion`, for the `suggestion` state variable and export it from the file
-
+export const selectSuggestion = (state) => state.suggestion.suggestion;
 export const selectLoading = (state) => state.suggestion.loading;
 export const selectError = (state) => state.suggestion.error;
